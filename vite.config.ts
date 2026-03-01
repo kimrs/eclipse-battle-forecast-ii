a66b372a16e90e3d965a0e17d67a6f7bb2061b67
kimru <kimru@users.noreply.github.com>
2026-03-01 16:46:33 +0100

    Added action

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/eclipse-battle-forecast-ii/',
   plugins: [react()],
   test: {
     environment: 'node',
